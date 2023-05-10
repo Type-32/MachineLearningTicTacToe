ed6778107ea3043a0cb7d844beb9bfe666157264
Type-32 <wilsonsu2008@hotmail.com>
2023-05-10 09:45:36 +0800

    Vue.js is my savior

--- tictactoeML/src/App.vue
+++ tictactoeML/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Main from "@/components/Main.vue";
 </script>
 
 <template>
+    <!--
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
@@ -11,9 +13,10 @@ import TheWelcome from './components/TheWelcome.vue'
       <HelloWorld msg="You did it!" />
     </div>
   </header>
+  -->
 
   <main>
-    <TheWelcome />
+    <Main />
   </main>
 </template>
 
---
+++ tictactoeML/src/components/Main.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {defineComponent,reactive,ref} from 'vue'
+
+//Make Tic Tac Toe game
+const counter = reactive({count: 0})
+const count = ref(0)
+const increment = () => {
+    counter.count++
+    count.value++
+}
+const bvalues = ref([" "," "," "," "," "," "," "," "," "])
+function turntable(index){
+    bvalues.value[index] = isturn.value ? "X" : "O"
+    isturn.value = !isturn.value
+}
+const isturn = ref(true)
+</script>
+
+<template>
+    <div>
+        <h1>Tic Tac Toe</h1>
+        <h3>..but with Machine Learning!</h3>
+        <br>
+        <h3>Status: {{isturn ? "X's turn" : "O's turn"}}</h3>
+    </div>
+    <main>
+        <input type="button" v-for="(text,index) in bvalues" :key="index" v-model="bvalues[index]" @click="turntable(index)"/>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: grid;
+    grid-template-columns: repeat(3, 50px);
+    grid-template-rows: repeat(3, 50px);
+    gap: 10px;
+}
+
+input[type="button"] {
+    margin: 0;
+}
+</style>
