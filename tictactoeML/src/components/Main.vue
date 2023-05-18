592e1e5be8d0613571d5923ca164551a50e275fc
Type-32 <wilsonsu2008@hotmail.com>
2023-05-18 12:23:48 +0800

    Changed sth

--- tictactoeML/src/components/Main.vue
+++ tictactoeML/src/components/Main.vue
@@ -2,7 +2,10 @@
 import {defineComponent,reactive,ref} from 'vue'
 import {NButton, NModal, NSpace, NSelect, NCard, NDivider, NInputNumber, useLoadingBar, useMessage} from 'naive-ui'
 import * as tf from '@tensorflow/tfjs';
+import * as fs from 'fs';
+import axios from 'axios';
 
+//Initialize Machine Learning Model
 const model = tf.sequential();
 model.add(tf.layers.dense({units: 9, inputShape: [9], activation: 'softmax'}));
 model.compile({loss: 'categoricalCrossentropy', optimizer: 'adam', metrics:['accuracy']});
@@ -19,6 +22,7 @@ const setEpochs = ref(100)
 //Modals
 const errorModal = ref(false)
 const trainingModal = ref(false)
+const showModal = ref(false)
 
 //Contents
 const applyIterationButton = ref(true)// true = enabled, false = disabled; These values are inverted on input at the template section.
@@ -71,75 +75,94 @@ const iterationData = ref([
     ],
     [
         //Preset Record Array Data - Iteration 3, Complementary Horizontal + Perpendicular
-        //Mid
-        [0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0],
+        //Perpendicular
+        [1,0,0,0,0,0,1,0,0],
+        [0,1,0,0,0,0,0,1,0],
+        [0,0,1,0,0,0,0,0,1],
+        [1,0,0,1,0,0,0,0,0],
+        [0,1,0,0,1,0,0,0,0],
+        [0,0,1,0,0,1,0,0,0],
+        [0,0,0,1,0,0,1,0,0],
+        [0,0,0,0,1,0,0,1,0],
+        [0,0,0,0,0,1,0,0,1],
 
-        //Top Left
-        [1,0,0,0,0,0,0,0,0],
-        [1,0,0,0,0,0,0,0,0],
-        [1,0,0,0,0,0,0,0,0],
-
-        //Top Right
-        [0,0,1,0,0,0,0,0,0],
-        [0,0,1,0,0,0,0,0,0],
-        [0,0,1,0,0,0,0,0,0],
-
-        //Bottom Right
-        [0,0,0,0,0,0,0,0,1],
-        [0,0,0,0,0,0,0,0,1],
-        [0,0,0,0,0,0,0,0,1],
-
-        //Bottom Left
-        [0,0,0,0,0,0,1,0,0],
-        [0,0,0,0,0,0,1,0,0],
-        [0,0,0,0,0,0,1,0,0],
-
-        //Mid Left
-        [0,0,0,1,0,0,0,0,0],
-        [0,0,0,1,0,0,0,0,0],
-
-        //Mid Right
-        [0,0,0,0,0,1,0,0,0],
-        [0,0,0,0,0,1,0,0,0]
+        //Horizontal
+        [1,0,1,0,0,0,0,0,0],
+        [0,0,0,1,0,1,0,0,0],
+        [0,0,0,0,0,0,1,0,1],
+        [1,1,0,0,0,0,0,0,0],
+        [0,0,0,1,1,0,0,0,0],
+        [0,0,0,0,0,0,1,1,0],
+        [0,1,1,0,0,0,0,0,0],
+        [0,0,0,0,1,1,0,0,0],
+        [0,0,0,0,0,0,0,1,1]
     ],
     [
         //Preset Record Array Data - Iteration 4, Complementary Diagonal
-        //Mid
-        [0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0],
-        [0,0,0,0,1,0,0,0,0],
+        //Top Left to Bottom Right
+        [1,0,0,0,0,0,0,0,1],
+        [1,0,0,0,1,0,0,0,0],
+        [0,0,0,0,1,0,0,0,1],
 
-        //Top Left
-        [1,0,0,0,0,0,0,0,0],
-        [1,0,0,0,0,0,0,0,0],
-        [1,0,0,0,0,0,0,0,0],
+        //Top Right to Bottom Left
+        [0,0,1,0,0,0,1,0,0],
+        [0,0,1,0,1,0,0,0,0],
+        [0,0,0,0,1,0,1,0,0]
+    ],
+    [
+        //Preset Record Array Data - Iteration 5, Optimal Complementary Horizontal + Perpendicular
+        //Angled
+        [1,0,1,0,0,0,0,1,0],
+        [1,0,0,0,0,1,1,0,0],
+        [0,1,0,0,0,0,1,0,1],
+        [0,0,1,1,0,0,0,0,1],
 
-        //Top Right
-        [0,0,1,0,0,0,0,0,0],
-        [0,0,1,0,0,0,0,0,0],
-        [0,0,1,0,0,0,0,0,0],
+        //Double Sided
+        [1,0,1,0,0,0,0,0,1],
+        [1,0,1,0,0,0,0,0,1],
 
-        //Bottom Right
-        [0,0,0,0,0,0,0,0,1],
-        [0,0,0,0,0,0,0,0,1],
-        [0,0,0,0,0,0,0,0,1],
+        [0,0,1,0,0,0,1,0,1],
+        [0,0,1,0,0,0,1,0,1],
 
-        //Bottom Left
-        [0,0,0,0,0,0,1,0,0],
-        [0,0,0,0,0,0,1,0,0],
-        [0,0,0,0,0,0,1,0,0],
+        [1,0,0,0,0,0,1,0,1],
+        [1,0,0,0,0,0,1,0,1],
 
-        //Mid Left
-        [0,0,0,1,0,0,0,0,0],
-        [0,0,0,1,0,0,0,0,0],
+        [1,0,1,0,0,0,1,0,0],
+        [1,0,1,0,0,0,1,0,0],
 
-        //Mid Right
-        [0,0,0,0,0,1,0,0,0],
-        [0,0,0,0,0,1,0,0,0]
+        //Quadruple Sided
+        [1,0,1,0,0,0,1,0,1],
+        [1,0,1,0,0,0,1,0,1],
+        [1,0,1,0,0,0,1,0,1],
+        [1,0,1,0,0,0,1,0,1],
+
+        //Cornered
+        [1,1,0,1,0,0,0,0,0],
+        [1,1,0,1,0,0,0,0,0],
+
+        [0,1,1,0,0,1,0,0,0],
+        [0,1,1,0,0,1,0,0,0],
+
+        [0,0,0,0,0,1,0,1,1],
+        [0,0,0,0,0,1,0,1,1],
+
+        [0,0,0,1,0,0,1,1,0],
+        [0,0,0,1,0,0,1,1,0],
+    ],
+    [
+        //Preset Record Array Data - Iteration 6, Optimal Complementary Diagonal
+        //Four Sided
+        [1,0,1,0,1,0,0,0,0],
+        [1,0,1,0,1,0,0,0,0],
+        [0,0,1,0,0,1,0,1,0],
+        [0,0,1,0,0,1,0,1,0],
+        [0,0,0,0,1,0,1,0,1],
+        [0,0,0,0,1,0,1,0,1],
+        [1,0,0,0,1,0,1,0,0],
+        [1,0,0,0,1,0,1,0,0],
+    ],
+    [
+
     ]
 ]);
 const iterationLabels = ref([
@@ -183,80 +206,96 @@ const iterationLabels = ref([
     ],
     [
         //Preset Record Labels - Iteration 3, Complementary Horizontal + Perpendicular
-        //Init Mid
-        [1,0,0,0,1,0,0,0,1],
-        [0,0,1,0,1,0,1,0,0],
+        //Init Perpendicular
+        [1,0,0,1,0,0,1,0,0],
         [0,1,0,0,1,0,0,1,0],
-        [0,0,0,1,1,1,0,0,0],
-
-        //Init Top Left
-        [1,0,0,1,0,0,1,0,0],
-        [1,0,0,0,1,0,0,0,1],
-        [1,1,1,0,0,0,0,0,0],
-
-        //Init Top Right
         [0,0,1,0,0,1,0,0,1],
-        [0,0,1,0,1,0,1,0,0],
-        [1,1,1,0,0,0,0,0,0],
-
-        //Init Bottom Right
+        [1,0,0,1,0,0,1,0,0],
+        [0,1,0,0,1,0,0,0,0],
         [0,0,1,0,0,1,0,0,1],
-        [1,0,0,0,1,0,0,0,1],
-        [0,0,0,0,0,0,1,1,1],
-
-        //Init Bottom Left
         [1,0,0,1,0,0,1,0,0],
-        [0,0,1,0,1,0,1,0,0],
+        [0,1,0,0,1,0,0,1,0],
+        [0,0,1,0,0,1,0,0,1],
+
+        //Init Horizontal
+        [1,1,1,0,0,0,0,0,0],
+        [0,0,0,1,1,1,0,0,0],
         [0,0,0,0,0,0,1,1,1],
-
-        //Init Mid Left
+        [1,1,1,0,0,0,0,0,0],
         [0,0,0,1,1,1,0,0,0],
-        [1,0,0,1,0,0,1,0,0],
-
-        //Init Mid Right
+        [0,0,0,0,0,0,1,1,1],
+        [1,1,1,0,0,0,0,0,0],
         [0,0,0,1,1,1,0,0,0],
-        [0,0,1,0,0,1,0,0,1]
+        [0,0,0,0,0,0,1,1,1]
     ],
     [
         //Preset Record Labels - Iteration 4, Complementary Diagonal
-        //Init Mid
+        //Init Top Left to Bottom Right
         [1,0,0,0,1,0,0,0,1],
-        [0,0,1,0,1,0,1,0,0],
-        [0,1,0,0,1,0,0,1,0],
-        [0,0,0,1,1,1,0,0,0],
-
-        //Init Top Left
-        [1,0,0,1,0,0,1,0,0],
         [1,0,0,0,1,0,0,0,1],
-        [1,1,1,0,0,0,0,0,0],
-
-        //Init Top Right
-        [0,0,1,0,0,1,0,0,1],
-        [0,0,1,0,1,0,1,0,0],
-        [1,1,1,0,0,0,0,0,0],
-
-        //Init Bottom Right
-        [0,0,1,0,0,1,0,0,1],
         [1,0,0,0,1,0,0,0,1],
-        [0,0,0,0,0,0,1,1,1],
 
-        //Init Bottom Left
-        [1,0,0,1,0,0,1,0,0],
+        //Init Top Right to Bottom Left
         [0,0,1,0,1,0,1,0,0],
-        [0,0,0,0,0,0,1,1,1],
+        [0,0,1,0,1,0,1,0,0],
+        [0,0,1,0,1,0,1,0,0]
+    ],
+    [
+        //Preset Record Labels - Iteration 5, Optimal Complementary Horizontal + Perpendicular
+        //Init Angled
+        [1,1,1,0,0,0,0,1,0],
+        [1,0,0,1,0,1,1,0,0],
+        [0,1,0,0,0,0,1,1,1],
+        [0,0,1,1,0,1,0,0,1],
 
-        //Init Mid Left
-        [0,0,0,1,1,1,0,0,0],
-        [1,0,0,1,0,0,1,0,0],
+        //Init Double Sided
+        [1,1,1,0,0,0,0,0,1],
+        [1,0,1,0,0,1,0,0,1],
 
-        //Init Mid Right
-        [0,0,0,1,1,1,0,0,0],
-        [0,0,1,0,0,1,0,0,1]
+        [0,0,1,0,0,0,1,1,1],
+        [0,0,1,0,0,1,1,0,1],
+
+        [1,0,0,0,0,0,1,1,1],
+        [1,0,0,1,0,0,1,0,1],
+
+        [1,1,1,0,0,0,1,0,0],
+        [1,0,1,1,0,0,1,0,0],
+
+        //Init Quadruple Sided
+        [1,1,1,0,0,0,1,0,1],
+        [1,0,1,0,0,1,1,0,1],
+        [1,0,1,0,0,0,1,1,1],
+        [1,0,1,1,0,0,1,0,1],
+
+        //Cornered
+        [1,1,1,1,0,0,0,0,0],
+        [1,1,0,1,0,0,1,0,0],
+
+        [1,1,1,0,0,1,0,0,0],
+        [0,1,1,0,0,1,0,0,1],
+
+        [0,0,0,0,0,1,1,1,1],
+        [0,0,1,0,0,1,1,0,1],
+
+        [0,0,0,1,0,0,1,1,1],
+        [1,0,0,1,0,0,1,1,0],
+    ],
+    [
+        //Preset Record Labels - Iteration 6, Optimal Complementary Diagonal
+        //Init Four Sided
+        [1,0,1,0,1,0,1,0,0],
+        [1,0,1,0,1,0,0,0,1],
+        [1,0,1,0,1,0,0,0,1],
+        [0,0,1,0,1,0,1,0,1],
+        [1,0,0,0,1,0,1,0,1],
+        [0,0,1,0,1,0,1,0,1],
+        [1,0,0,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0,0],
+    ],
+    [
+
     ]
 ]);
-//Preset Record Array Data - Iteration 5, Optimal Complementary Horizontal + Perpendicular
-//Preset Record Array Data - Iteration 6, Optimal Complementary Diagonal
-
 
 //Training Data Container
 const trainingData = ref([]);
@@ -268,7 +307,7 @@ const iterationSelection = ref([
         value:1
     },
     {
-        label:"Model Iteration 2 (\"Gogh\" Epoch 2)",
+        label:"Model Iteration 2 (\"Da Vinci\" Epoch 2)",
         value:2
     },
     {
@@ -276,18 +315,42 @@ const iterationSelection = ref([
         value:3
     },
     {
-        label:"Model Iteration 4 (\"Turing\" Epoch 4)",
+        label:"Model Iteration 4 (\"Atomic\" Epoch 4)",
         value:4
     },
     {
-        label:"Model Iteration 5 (\"Shannon\" Epoch 5)",
+        label:"Model Iteration 5 (\"Watson\" Epoch 5)",
         value:5
     },
     {
-        label:"Model Iteration 6 (\"Polar\" Epoch 6)",
+        label:"Model Iteration 6 (\"Bohr\" Epoch 6)",
         value:6
     },
+    {
+        label:"Model Iteration 7 (\"Schrödinger\" Epoch 7)",
+        value:7
+    }
 ]);
+const correctionMethod = ref(1)
+const correctionMethodSelect = ref([
+    {
+        label:"Russian Roulette", //Random Selection & Accumulative Index Selection Method
+        value:1
+    },
+    {
+        label:"Assimilation", //Assimilate all boardValues into 1
+        value:2
+    },
+    {
+        label:"Reverse Assimilation", //Assimilate all boardValues into 2
+        value:3
+    },
+    {
+        label:"Aversion", //Inverse all boardValues from 1 to 2 and 2 to 1
+        value:4
+    }
+]);
+const correctionThreshold = ref(10)
 
 async function applyIteration(){
     iterationSelectBar.value = false;
@@ -456,6 +519,15 @@ function invertBoardArray(tmpArr){
     }
     return tmpArr
 }
+function assimilateBoardArray(referenceIndex:number){
+    let tmpArr = scanBoard()
+    for(let i = 0; i < tmpArr.length; i++){
+        if(tmpArr[i] != 0){
+            tmpArr[i] = referenceIndex
+        }
+    }
+    return tmpArr
+}
 function turntable(index, reportError:boolean = true){
     if(bvalues.value[index] != " "){
         if(reportError) errorModal.value = true
@@ -476,6 +548,40 @@ function turntable(index, reportError:boolean = true){
     //recordLabelsData(invertRecordData(modelValues))
     return true
 }
+function writeModel(){
+    showModal.value = true
+    let it7Data = JSON.parse(fs.readFileSync('data.json','utf-8'));
+    it7Data.push({
+        "trainingData": prevModelValues.value,
+        "labelsData": modelValues.value
+    })
+    axios.post("/path/to/data.json",it7Data)
+        .then(response =>{
+            console.log(response)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    fs.writeFileSync('data.json',JSON.stringify(it7Data))
+}
+function readModel(){
+    let it7Data;
+    axios.get("/path/to/data.json")
+        .then(response =>{
+            it7Data = JSON.parse(response.data);
+            for(let i = 0; i < it7Data.length; i++){
+                for(let j = 0; j < it7Data[i].trainingData.length; j++){
+                    iterationData.value[6].push(it7Data[i].trainingData[j])
+                    iterationLabels.value[6].push(it7Data[i].labelsData[j])
+                }
+            }
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    return it7Data
+}
+readModel()
 function resetGame(){
     for (let i = 0; i < bvalues.value.length; i++) {
         bvalues.value[i] = " "
@@ -521,26 +627,47 @@ async function trainModel(announceMessage: boolean = true) {
 const prediction = ref('')
 const argDef = ref(-1)
 function makePrediction() {
-    //trainModel()
+    function generatePredictionIndex() {
+        // Define the game board state
+        let gameState;
+        if (correctionMethod.value == 1) {
+            gameState = tf.tensor2d([scanBoard()]);
+        } else if (correctionMethod.value == 2) {
+            gameState = tf.tensor2d([assimilateBoardArray(1)]);
+        } else if (correctionMethod.value == 3) {
+            gameState = tf.tensor2d([assimilateBoardArray(2)]);
+        } else if (correctionMethod.value == 4) {
+            gameState = tf.tensor2d([invertBoardArray(scanBoard())]);
+        } else {
+            gameState = tf.tensor2d([scanBoard()]);
+        }
+
+
+        // Use the model to predict the next move
+        const output = model.predict(gameState);
+
+        // Get the predicted move
+        predictionIndex.value = output.argMax(-1).dataSync()[0];
+        prediction.value = `(${Math.floor(predictionIndex.value / 3)}, ${predictionIndex.value % 3})`;
+        console.log(prediction)
+    }
     if (!model) return;
 
-    // Define the game board state
-    const gameState = tf.tensor2d([scanBoard()]);
-
-    // Use the model to predict the next move
-    const output = model.predict(gameState);
-
-    // Get the predicted move
-    predictionIndex.value = output.argMax(-1).dataSync()[0];
-    prediction.value = `(${Math.floor(predictionIndex.value / 3)}, ${predictionIndex.value % 3})`;
-    console.log(prediction)
+    generatePredictionIndex()
     let val = turntable(predictionIndex.value,false)
     let cnt = 0
     while (!val){
-        console.log((predictionIndex.value + cnt) % 9)
-        val = turntable((predictionIndex.value + cnt) % 9,false)
+        if(correctionMethod.value == 1) {
+            //console.log((predictionIndex.value + cnt) % 9)
+            val = turntable((predictionIndex.value + cnt) % 9, false)
+        }else{
+            generatePredictionIndex()
+            //console.log((predictionIndex.value + cnt) % 9)
+            val = turntable((predictionIndex.value + cnt) % 9, false)
+        }
+
         cnt += 1
-        if(cnt > 9){
+        if(cnt > correctionThreshold.value){
             console.log("No more moves available")
             break
         }
@@ -574,6 +701,17 @@ trainedTimes.value = model.getWeights().length
             title="Results"
             content="Model Training Complete!"
         />
+    <NModal
+            v-model:show="showModal"
+            :mask-closable="true"
+            preset="dialog"
+            title="Current Model"
+    >
+        <p>Training Data:</p>
+        <p>{{modelValues}}</p>
+        <p>Labels:</p>
+        <p>{{prevModelValues}}</p>
+    </NModal>
     <NSpace vertical class="middle-align">
 
         <!--Title Space-->
@@ -585,18 +723,30 @@ trainedTimes.value = model.getWeights().length
         <!--Selection Space-->
         <NCard title="Model Options" class="middle-align" v-if="mainContent === false" size="medium">
             <p>Iteration Model</p>
-            <NSpace align="center" justify="center">
-                <NSelect :disabled="!iterationSelectBar" v-model:value="iterations" :options="iterationSelection" :consistent-menu-width="false"/>
-                <NButton type="primary" :disabled="!applyIterationButton" @click="applyIteration">Set Iteration</NButton>
-            </NSpace>
+            <NSelect :disabled="!iterationSelectBar" v-model:value="iterations" :options="iterationSelection" :consistent-menu-width="false"/>
             <br>
             <p>Epoch Amount for Training</p>
             <NInputNumber
+                :disabled="!iterationSelectBar"
                 v-model:value="setEpochs"
                 placeholder="Integer Value, 100~5000"
                 :min="100"
                 :max="5000"
             />
+            <br>
+            <p>Model Correction Method</p>
+            <NSelect :disabled="!iterationSelectBar" v-model:value="correctionMethod" :options="correctionMethodSelect" :consistent-menu-width="false"/>
+            <br>
+            <p>Correction Number Threshold</p>
+            <NInputNumber
+                :disabled="!iterationSelectBar"
+                v-model:value="correctionThreshold"
+                placeholder="Integer Value, 10~50"
+                :min="10"
+                :max="50"
+            />
+            <br>
+            <NButton type="primary" :disabled="!applyIterationButton" @click="applyIteration">    Set Iteration    </NButton>
         </NCard>
 
         <!--Main Space-->
@@ -605,8 +755,10 @@ trainedTimes.value = model.getWeights().length
                 <NButton type="primary" @click="resetGame" id="reset-game-button" v-show="matchFinished">Reset Game</NButton>
                 <NButton type="primary" @click="trainModel" id="train-model-button" v-show="matchFinished">Train Model</NButton>
                 <!--<NButton @click="initialize" class="" id="reset-game-button">Initialize Model</NButton>-->
+                <NButton type="primary" id="show-model-button" v-show="matchFinished" @click="writeModel">Write Model</NButton>
                 <NButton type="primary" @click="makePrediction" id="ML-predict-button" v-show="!matchFinished">Make AI Prediction</NButton>
                 <!--<NButton @click="clearModelWeights" id="clear-model-button">Clear Model</NButton>-->
+
                 <NButton type="primary" @click="returnToSelection" id="return-to-selection-button">Switch Model</NButton>
             </NSpace>
             <NSpace vertical>
@@ -690,13 +842,20 @@ NButton{
 h1{
     font-size: 50px;
     font-weight: bold;
+    margin-bottom: 0px;
 }
 h3{
     font-size: 20px;
     font-weight: bold;
+    margin-bottom: 0px;
 }
 h4{
     font-size: 15px;
     font-weight: bold;
+    margin-bottom: 0px;
+}
+p{
+    font-size: 15px;
+    margin-bottom: 3px;
 }
 </style>
